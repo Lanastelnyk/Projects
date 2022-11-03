2372c1df2d4a01d6c9a73cc68786c97cb1e6ae45
Lanastelnyk <lanastelnik@gmail.com>
2022-11-03 14:07:00 -0500

    HW2 JS

---
+++ HomeWork2 JS/main.js
@@ -0,0 +1,50 @@
+let arr= [10,20,true,'stringa',[1,2,3,],false,30,40,50,[4,5,6,7]];
+console.log(arr);
+console.log (arr [0]);
+console.log (arr [1]);
+console.log (arr [2]);
+console.log (arr [3]);
+console.log (arr [4]);
+console.log (arr [5]);
+console.log (arr [6]);
+console.log (arr [7]);
+console.log (arr [8]);
+console.log (arr [9]);
+
+let book1 = {title:'HarryPotter', pageCount:40, genre:'action'}
+let book2 = {title:'Moon', pageCount:50, genre:'comedi'}
+let book3 = {title:'Workbook', pageCount:60, genre:'historical'}
+console.log (book3.genre)
+
+
+book1. authors = ['name:Gellen' ,'age:40']
+book2. authors = ['name:Tom' ,'age:50']
+book3. authors = ['name:Ivan' ,'age:60']
+
+console.log(book1)
+let user=[
+    {name:'Liam', username:'Noah', password: 12345},
+    {name:'Oliver', username:'Ozzy', password: 67891},
+    {name:'Elijah', username:'Loyal', password: 12345},
+    {name:'James', username:'Jiraiya', password: 12568},
+    {name:'William', username:'Elio', password: 85224},
+    {name:'Benjamin', username:'Noah', password: 65215},
+    {name:'Lucas', username:'Noah', password: 28451},
+    {name:'Henry', username:'Noah', password: 24515},
+    {name:'Theodore', username:'Noah', password: 25876},
+    {name:'Camilo', username:'Noah', password: 65266},
+    {name:'Camilo', username:'Noah', password: 65266},
+
+]
+
+console.log(user[0].password)
+console.log(user[1].password)
+console.log(user[2].password)
+console.log(user[3].password)
+console.log(user[4].password)
+console.log(user[5].password)
+console.log(user[6].password)
+console.log(user[7].password)
+console.log(user[8].password)
+console.log(user[9].password)
+console.log(user[10].password)
